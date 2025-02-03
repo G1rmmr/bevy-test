d0ebad1feed9316d309f60f77f9e49eee6fe4c02
G1rmmr <52289794+G1rmmr@users.noreply.github.com>
2025-02-04 00:29:06 +0900

    FIX

--- src/player.rs
+++ src/player.rs
@@ -4,11 +4,8 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-<<<<<<< HEAD
-        .add_system(camera_movement_system) // 카메라 이동 시스템 등록
-=======
         .add_system(camera_movement_system)
->>>>>>> master
+        .add_system(camera_movement_system)
         .run();
 }
 
@@ -24,10 +21,6 @@ fn setup(mut commands: Commands) {
         FPSCamera,
     ));
 
-<<<<<<< HEAD
-    // 예시로 3D 큐브를 하나 추가
-=======
->>>>>>> master
     commands.spawn_bundle(PbrBundle {
         mesh: bevy::prelude::Mesh::from(shape::Cube { size: 2.0 }),
         material: StandardMaterial {
@@ -38,10 +31,6 @@ fn setup(mut commands: Commands) {
         ..Default::default()
     });
 
-<<<<<<< HEAD
-    // 간단한 조명 추가
-=======
->>>>>>> master
     commands.spawn_bundle(PointLightBundle {
         point_light: PointLight {
             intensity: 1500.0,
@@ -53,10 +42,6 @@ fn setup(mut commands: Commands) {
     });
 }
 
-<<<<<<< HEAD
-// 카메라 이동을 위한 시스템
-=======
->>>>>>> master
 fn camera_movement_system(
     time: Res<Time>,
     keyboard_input: Res<Input<KeyCode>>,
@@ -65,20 +50,12 @@ fn camera_movement_system(
     let mut direction = Vec3::ZERO;
     let speed = 5.0;
 
-<<<<<<< HEAD
-    // W/S: 전후방 이동 (z축)
-=======
->>>>>>> master
     if keyboard_input.pressed(KeyCode::W) {
         direction.z -= 1.0;
     }
     if keyboard_input.pressed(KeyCode::S) {
         direction.z += 1.0;
     }
-<<<<<<< HEAD
-    // A/D: 좌우 이동 (x축)
-=======
->>>>>>> master
     if keyboard_input.pressed(KeyCode::A) {
         direction.x -= 1.0;
     }
@@ -86,18 +63,10 @@ fn camera_movement_system(
         direction.x += 1.0;
     }
 
-<<<<<<< HEAD
-    // 방향 벡터가 0이 아니라면 정규화하여 일정 속도로 이동
-=======
->>>>>>> master
     if direction != Vec3::ZERO {
         direction = direction.normalize();
     }
 
-<<<<<<< HEAD
-    // 모든 카메라에 대해 이동 적용
-=======
->>>>>>> master
     for mut transform in query.iter_mut() {
         transform.translation += direction * speed * time.delta_seconds();
     }
